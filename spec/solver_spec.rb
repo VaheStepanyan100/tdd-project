5a078ab11b187db2aebf0da1b33d5d47672f98b6
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-08-11 12:04:58 +0400

    Add additional test cases to Solver class

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -9,5 +9,19 @@ describe Solver do
     it 'returns 1 for 0' do
       expect(@solver.factorial(0)).to eq 1
     end
+
+    it 'returns 1 for 1' do
+      expect(@solver.factorial(1)).to eq 1
+    end
+
+    it 'returns correct factorial for positive numbers' do
+      expect(@solver.factorial(5)).to eq 120
+      expect(@solver.factorial(10)).to eq 3_628_800
+    end
+
+    it 'raises Argument Error for negative numbers' do
+      expect { @solver.factorial(-5) }.to raise_error(ArgumentError)
+      expect { @solver.factorial(-10) }.to raise_error(ArgumentError)
+    end
   end
 end
