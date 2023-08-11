798c3da76ac0b0054376a08d772f04dcc1b417c6
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-08-11 12:10:22 +0400

    Check to raise ArgumentError and use loop to calculate result

--- solver.rb
+++ solver.rb
@@ -1,5 +1,10 @@
 class Solver
   def factorial(int)
+    raise ArgumentError, 'Factorial is not defined for negative numbers.' if int.negative?
     return 1 if int.zero?
+
+    result = 1
+    (1..int).each { |i| result *= i }
+    result
   end
 end
